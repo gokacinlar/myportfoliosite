81c792e457119c16a768beb706c05941c5fde4f3
gokacinlar <gokacinlarthedissident@gmail.com>
2023-12-15 02:45:44 +0300

    major changes...

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,16 @@
+// progress bar
+
+window.onscroll = function () {
+    pBarFunc();
+};
+
+function pBarFunc() {
+    var winScroll = window.scrollY || document.documentElement.scrollTop;
+    var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    var scrolled = (winScroll / height) * 100;
+    document.getElementById("myBar").style.width = scrolled + "%";
+}
+
 // slide carousel
 
 let slideIndex = 0;
@@ -34,3 +47,55 @@ function updateIndicators(index) {
 }
 
 showSlide(slideIndex);
+
+// header dropdown
+
+function dropDownFunction() {
+    document.getElementById("dropdown").classList.toggle("show");
+    var icon = document.querySelector('.header-dropdown .bi');
+    if (icon.classList.contains('bi-list')) {
+        icon.classList.replace('bi-list', 'bi-x');
+    } else {
+        icon.classList.replace('bi-x', 'bi-list');
+    }
+}
+
+window.onclick = function (e) {
+    if (!e.target.matches('.dropbtn')) {
+        var myDropdown = document.getElementById("myDropdown");
+        if (myDropdown.classList.contains('show')) {
+            myDropdown.classList.remove('show');
+        }
+    }
+}
+
+
+// nav dropdown
+
+const menuIcon = document.getElementById("menu-icon");
+const navRight = document.getElementById("nav-right");
+
+menuIcon.addEventListener("click", toggleNav);
+
+function toggleNav() {
+    navRight.classList.toggle("show");
+}
+
+// scroll to top button
+
+let mybutton = document.getElementById("scrollTopBtn");
+
+window.onscroll = function () { scrollFunction() };
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+}
+
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
