7089dceb170cc18335c91ddc73f9cc556fd729a4
gokacinlar <101329870+gokacinlar@users.noreply.github.com>
2024-07-13 03:17:50 +0300

    add: custom 404 page

---
+++ 404.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta http-equiv="Content-Security-Policy"
+        content="script-src 'self' 'unsafe-eval' https://www.googletagmanager.com/gtag/js?id=G-5G6Z79KR5H 'unsafe-inline'; object-src 'self'">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="owner" content="Derviş Öksüzoğlu">
+    <meta name="keywords"
+        content="derviş, öksüzoğlu, projeler, portfolyo, blog, pedagoji, felsefe, web geliştirme, psikoloji">
+    <meta name="description"
+        content="Derviş Öksüzoğlu | Projeler: İngilizce Öğretmeni & Front-end Web Geliştiricisi; Pedagoji, Web Geliştirme, Felsefe ve Dahası">
+    <meta name="referrer" content="no-referrer-when-downgrade">
+    <link rel="canonical" href="https://www.dervisoksuzoglu.net">
+    <!--favicon-->
+    <link rel="icon" type="image/webp" sizes="32x32" href="assets/img/title_logos/titlelogo_32x32.webp">
+    <link rel="icon" type="image/webp" sizes="16x16" href="assets/img/title_logos/titlelogo_16x16.webp">
+    <!--external css-->
+    <link rel="stylesheet" type="text/css" href="assets/css/index.css" as="style">
+    <title>404 - Bulunamadı</title>
+</head>
+
+<body id="notFoundPageBody">
+    <main>
+        <div>
+            <div class="notFound-picture">
+                <picture class="responsive-img php-notfound-img">
+                    <source srcset="assets/img/main/res/myself-small.webp" media="(max-width: 300px)">
+                    <source srcset="assets/img/main/res/myself-medium.webp" media="(max-width: 768px)">
+                    <img src="assets/img/main/myself.webp" alt="Derviş Öksüzoğlu">
+                </picture>
+            </div>
+            <div>
+                <div class="notFound-actions">
+                    <div>
+                        <h1>Aradığınız sayfa bulunamadı!</h1>
+                        <p>Hata kodu:
+                        <pre><code>404</code></pre>
+                        </p>
+                    </div>
+                    <div class="notFound-return">
+                        <a href="index.html">Geri Dön</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
+</body>
+
+</html>
