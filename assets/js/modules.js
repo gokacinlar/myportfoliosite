bf1a9557d378e0c017dc481e91791838efda810f
gokacinlar <101329870+gokacinlar@users.noreply.github.com>
2024-08-06 23:02:32 +0300

    Beautify modules.

--- assets/js/modules.js
+++ assets/js/modules.js
@@ -1,5 +1,13 @@
+// jQuery
+import '../lib/js/jquery-3.7.1.min.js';
+
+// Libraries
+import '../lib/js/lazysizes.min.js';
+
+// Components
 import './components/header.js';
 import './components/footer.js';
 import './components/carousel.js';
 import './components/project-navigation.js';
 import './components/separator.js';
+import './components/showcasePosts.js';
