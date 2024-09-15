809347692b769ec6d54e6611280d242a696b7950
gokacinlar <101329870+gokacinlar@users.noreply.github.com>
2024-09-15 22:33:20 +0300

    Fix file path issues.

--- assets/js/modules.js
+++ assets/js/modules.js
@@ -1,13 +1,14 @@
 // jQuery
-import '../lib/js/jquery-3.7.1.min.js';
+import '/assets/lib/js/jquery-3.7.1.min.js';
 
 // Libraries
-import '../lib/js/lazysizes.min.js';
+import '/assets/lib/js/lazysizes.min.js';
 
 // Components
-import './components/header.js';
-import './components/footer.js';
-import './components/carousel.js';
-import './components/project-navigation.js';
-import './components/separator.js';
-import './components/showcasePosts.js';
+import '/assets/js/components/header.js';
+import '/assets/js/components/footer.js';
+import '/assets/js/components/carousel.js';
+import '/assets/js/components/project-navigation.js';
+import '/assets/js/components/separator.js';
+import '/assets/js/components/showcasePosts.js';
+import '/assets/js/components/landing.js';
